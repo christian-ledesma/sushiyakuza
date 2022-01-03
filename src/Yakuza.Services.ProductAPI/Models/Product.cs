67acc31fc9ee295b95e659b7d5a6811d4cc8f530
Christian <christian_ledesma_a@hotmail.com>
2022-01-03 23:37:05 +0100

    Product data type modification

--- src/Yakuza.Services.ProductAPI/Models/Product.cs
+++ src/Yakuza.Services.ProductAPI/Models/Product.cs
@@ -9,7 +9,7 @@ namespace Yakuza.Services.ProductAPI.Models
         [Required]
         public string Name { get; set; }
         [Range(1, 1000)]
-        public int Price { get; set; }
+        public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
